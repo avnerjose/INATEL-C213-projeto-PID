7cf41af9218c8e0ac76c223fc46edf566321528f
fabiofiorita <fabiolfp@gmail.com>
2022-09-21 15:32:59 -0300

    Calculos do metodo CHR

---
+++ calculoCHR.m
@@ -0,0 +1,18 @@
+k = 5;
+t = 146.1;
+o = 11.9;
+sys = tf([5], [146.1, 1], 'InputDelay', 11.9);
+
+Kp = (0.95*t)/(k*o)
+
+Ti = 1.357*t
+
+Td = 0.473*o
+
+PIDCHRcom = pidstd(Kp,Ti,Td);
+
+RespostaCHRcom = feedback(sys*PIDCHRcom,1);
+
+grid on
+step(RespostaCHRcom)
+legend('CHRcom')
